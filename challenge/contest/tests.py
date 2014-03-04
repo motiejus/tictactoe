6499041f20978d996dd3cef1e23439a6d1ecf894
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-03-04 12:57:34 +0100

    Index test case

--- challenge/contest/tests.py
+++ challenge/contest/tests.py
@@ -1,7 +1,16 @@
 import doctest
+
+from django.test import TestCase
+
 from challenge.contest import logic
 
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(logic))
     return tests
+
+
+class ViewTestCase(TestCase):
+    def test_index(self):
+        request = self.client.get('/')
+        self.assertEqual(request.status_code, 200)
