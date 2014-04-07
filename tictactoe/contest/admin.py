685979578b412971838f0f837b9d34491f434411
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-07 16:52:05 +0200

    Contest admin interface

---
+++ tictactoe/contest/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Entry, LatestEntry, Fight
+
+
+class LatestEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'entry')
+
+
+class EntryAdmin(admin.ModelAdmin):
+    actions = ['force_entry_qualification']
+    list_display = ('user', 'codesize', 'uploaded')
+
+    def force_entry_qualification(self, request, queryset):
+        for e in queryset:
+            e.qualify()
+            self.message_user(request, "qualification rescheduled for %s." % e)
+
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(LatestEntry, LatestEntryAdmin)
+admin.site.register(Fight)
