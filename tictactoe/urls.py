346fcac72b51215b2b6777e1d635bb6cb8f84de8
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-07 19:58:09 +0200

    Remove slashes from urls

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -7,12 +7,12 @@ urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='contest/index.html')),
 
-    url(r'^upload/$', 'tictactoe.contest.views.upload'),
-    url(r'^entry/(?P<id>\d+)/$', 'tictactoe.contest.views.entry'),
-    url(r'^entries/$', 'tictactoe.contest.views.entries'),
+    url(r'^upload$', 'tictactoe.contest.views.upload'),
+    url(r'^entry/(?P<id>\d+)$', 'tictactoe.contest.views.entry'),
+    url(r'^entries$', 'tictactoe.contest.views.entries'),
 
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
+    url(r'^login$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout',
         {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 
