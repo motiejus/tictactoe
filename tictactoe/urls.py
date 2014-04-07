cf4f1b6ab8473406ed7ff5a906e85bb29dced958
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-07 23:17:31 +0200

    FIX login url

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     url(r'^entries$', 'tictactoe.contest.views.entries'),
     url(r'^fight/(?P<id>\d+)$', 'tictactoe.contest.views.fight'),
 
-    url(r'^login$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout',
         {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
