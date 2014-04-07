12846165addec55adb767d247428b6722f28d5bb
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-07 16:51:22 +0200

    Fights between entries are unique (overwrite)

--- tictactoe/contest/migrations/0002_fight.py
+++ tictactoe/contest/migrations/0002_fight.py
@@ -12,14 +12,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Fight',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('x', models.ForeignKey(to='contest.Entry', to_field='id')),
-                ('o', models.ForeignKey(to='contest.Entry', to_field='id')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('x', models.ForeignKey(to_field='id', to='contest.Entry')),
+                ('o', models.ForeignKey(to_field='id', to='contest.Entry')),
                 ('gameplay', models.CommaSeparatedIntegerField(max_length=230, help_text="Gameplay flow. Board is separated to 81 cells. Each number means a move by alternating player. For example, '10,1,0' means: x placed (2,1,1,1), o placed (1,1,1,1) and x made an error. In case 0 is at the end (like in the example), 'error' field is non-empty.")),
                 ('error', models.CharField(max_length=255, help_text="Non-empty if `gameplay' ends with zero", blank=True)),
                 ('result', models.CharField(max_length=10, help_text='Fight result of x (e1) versus o (e2). Relative to e1.', choices=[('win', 'win'), ('draw', 'draw'), ('loss', 'loss')])),
             ],
             options={
+                'unique_together': set([('x', 'o')]),
                 'index_together': set([('x', 'result'), ('o', 'result')]),
             },
             bases=(models.Model,),
--- tictactoe/contest/migrations/0003_latestentry.py
+++ tictactoe/contest/migrations/0003_latestentry.py
@@ -6,19 +6,20 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contest', '0002_fight'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contest', '0002_fight'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='LatestEntry',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, to_field='id')),
-                ('entry', models.ForeignKey(to='contest.Entry', to_field='id')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('user', models.ForeignKey(to_field='id', to=settings.AUTH_USER_MODEL)),
+                ('entry', models.ForeignKey(to_field='id', to='contest.Entry')),
             ],
             options={
+                'verbose_name_plural': 'Latest Entries',
             },
             bases=(models.Model,),
         ),
