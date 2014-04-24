8e65783f87783613bd51e673507c3372ab098920
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-24 07:19:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name='tictactoe',
-    version='0.0.3',
+    version='0.0.4',
     author="Motiejus Jakštys",
     author_email="desired.mta@gmail.com",
     description="Ultimate Tic-Tac-Toe tictactoe front-end",
